e26f900a110e9c8126223f1e227956560ff5ca7b
Dishan Chulawansa <dishan.chulawansa@gflowplus.com>
2024-12-03 15:50:35 +0530

    Add project files.

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private List<CityWeather> cities = new List<CityWeather>()
+        {
+            new CityWeather 
+            { 
+                CityUniqueCode = "LDN",
+                CityName = "London",
+                DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
+                TemperatureFahrenheit = 33 
+            },
+
+            new CityWeather 
+            { 
+                CityUniqueCode = "NYC",
+                CityName = "New York",
+                DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
+                TemperatureFahrenheit = 60 
+            },
+
+            new CityWeather
+            { 
+                CityUniqueCode = "PAR",
+                CityName = "Paris",
+                DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
+                TemperatureFahrenheit = 82 
+            }
+        };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cities);
+        }
+
+        [Route("weather/{citycode?}")]
+        public IActionResult City(string? citycode)
+        {
+            if (string.IsNullOrEmpty(citycode))
+            {
+                return View();
+            }
+            CityWeather? city = cities.Where(temp => temp.CityUniqueCode == citycode).FirstOrDefault();
+            return View(city);
+        }
+    }
+}
